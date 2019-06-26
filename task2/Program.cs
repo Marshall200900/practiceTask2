40248edbacb6a869a35b3d0cfaa98f7da0172cd5
Арсений Сидоров <aasidorov_6@edu.hse.ru>
2019-06-26 13:33:43 +0500

    deleted unused methods

--- task2/Program.cs
+++ task2/Program.cs
@@ -37,56 +37,26 @@ namespace task2
             while (!ok);
             return array;
         }
-        public static double Double(string sentence, double minBorder = double.MinValue, double maxBorder = double.MaxValue)
-        {
-            double result = 0;
-            bool ok = true;
-            do
-            {
-                Console.Write(sentence);
-                ok = double.TryParse(Console.ReadLine(), out result);
-                if (result < minBorder || result > maxBorder)
-                {
-                    ok = false;
-                }
-            }
-            while (!ok);
-            return result;
-        }
-        public static int Int(string sentence, int minBorder = int.MinValue, int maxBorder = int.MaxValue)
-        {
-            int result = 0;
-            bool ok = true;
-            do
-            {
-                Console.Write(sentence);
-                ok = int.TryParse(Console.ReadLine(), out result);
-                if (result < minBorder || result > maxBorder)
-                {
-                    ok = false;
-                }
-            }
-            while (!ok);
-            return result;
-        }
         static void Main(string[] args)
         {
-            int[] input = IntList("", 2);
-            int robots = input[0];
-            int years = input[1];
-            int[] array = new int[years + 1];
-            array[1] = robots;
-            int sum = 0;
-            for(int i = 2; i < years + 1; i++)
+            int[] input = IntList("", 2); //Ввод количества роботов и года
+            int robots = input[0]; //Присвоение роботов
+            int years = input[1]; //Присвоение годов
+            int[] array = new int[years + 1]; //Массив количества сделанных роботов на каждом году
+            array[1] = robots; // На первом году было "robots" роботов
+            int sum = 0; //Сумма роботов
+            for(int i = 2; i < years + 1; i++) // Цикл от двух до years
             {
-                sum = 0;
-                for(int x = i-1; x> i - 4; x--)
+                sum = 0; 
+                for(int x = i-1; x> i - 4; x--) // Сумирование роботов за предыдущие три года
                 {
                     if (x >= 1)
                         sum += array[x];
                 }
                 int max = 0;
-                for(int n3 = 0; n3 < 5; n3++)
+                
+                for(int n3 = 0; n3 < 5; n3++) //Подсчет максимально возможного количества сделанных роботов
+                    //Подсчет идет от 0 до 4, так как больше 4 групп по три робота делать нецелесообразно
                 {
                     if (n3 * 3 <= sum)
                     {
